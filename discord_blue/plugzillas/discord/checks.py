4a5150bc447d59d863f31aa52936b92b55cdc16f
Chris Busillo <info@shinycomputers.com>
2023-10-08 23:54:07 -0400

    create group of texty channels

--- discord_blue/plugzillas/discord/checks.py
+++ discord_blue/plugzillas/discord/checks.py
@@ -5,6 +5,16 @@ from discord_blue.config import config
 
 logger = logging.getLogger(__name__)
 
+TEXT_CHANNELS = (
+    discord.TextChannel,
+    discord.StageChannel,
+    discord.DMChannel,
+    discord.GroupChannel,
+    discord.VoiceChannel,
+    discord.StageChannel,
+    discord.Thread,
+)
+
 
 def has_employee_role():  # type: ignore[arg-type]
     async def predicate(interaction: discord.Interaction) -> bool:
