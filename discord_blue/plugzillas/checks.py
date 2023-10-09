94b08a93978e671e70bf523fe95d66b9f9d44304
Chris Busillo <info@shinycomputers.com>
2023-10-08 20:51:39 -0400

    new checks

---
+++ discord_blue/plugzillas/checks.py
@@ -0,0 +1,17 @@
+import logging
+import discord
+from discord import app_commands
+from discord.ext import commands
+from discord_blue.config import config
+
+logger = logging.getLogger(__name__)
+
+
+def has_employee_role():  # type: ignore
+    async def predicate(interaction: discord.Interaction) -> bool:
+        if isinstance(interaction.user, discord.Member):
+            result = any(role.name == config.discord.employee_role_name for role in interaction.user.roles)
+            return result
+        return False
+
+    return app_commands.check(predicate)
