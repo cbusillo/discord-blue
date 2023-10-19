06927cbb4c27b98bbf1e295d24a0f6c2b224d7cf
Chris Busillo <info@shinycomputers.com>
2023-10-19 19:55:43 -0400

    better var names

--- discord_blue/plugzillas/barcode_encoder_plug.py
+++ discord_blue/plugzillas/barcode_encoder_plug.py
@@ -3,54 +3,56 @@ class BarcodeEncoder:
 
     def encode_128(self, string_input: str) -> str:
         def only_digits(string_input_to_digits: str) -> int:
-            return all(c.isdigit() for c in string_input_to_digits)
+            return all(current_character.isdigit() for current_character in string_input_to_digits)
 
-        bytes_output = [0] * 256
-        idx = 0
-        i = 0
+        byte_array = [0] * 256
+        byte_index = 0
+        current_position = 0
         table_b = True
 
-        while i < len(string_input):
+        while current_position < len(string_input):
             if table_b:
-                if i + 4 <= len(string_input) and only_digits(string_input[i : i + 4]):
-                    if i == 0:
-                        bytes_output[idx] = 105
+                if current_position + 4 <= len(string_input) and only_digits(string_input[current_position : current_position + 4]):
+                    if current_position == 0:
+                        byte_array[byte_index] = 105
                     else:
-                        bytes_output[idx] = 99
-                    idx += 1
+                        byte_array[byte_index] = 99
+                    byte_index += 1
                     table_b = False
                 else:
-                    if i == 0:
-                        bytes_output[idx] = 104
-                        idx += 1
+                    if current_position == 0:
+                        byte_array[byte_index] = 104
+                        byte_index += 1
 
             if not table_b:
-                mini = 2
-                if i + mini <= len(string_input) and only_digits(string_input[i : i + mini]):
-                    dummy = int(string_input[i : i + 2])
-                    i += 2
+                digit_chunk_length = 2
+                if current_position + digit_chunk_length <= len(string_input) and only_digits(
+                    string_input[current_position : current_position + digit_chunk_length]
+                ):
+                    digit_value = int(string_input[current_position : current_position + 2])
+                    current_position += 2
                 else:
-                    dummy = 100
+                    digit_value = 100
                     table_b = True
-                bytes_output[idx] = dummy
-                idx += 1
+                byte_array[byte_index] = digit_value
+                byte_index += 1
             if table_b:
-                bytes_output[idx] = ord(string_input[i]) - 32
-                idx += 1
-                i += 1
+                byte_array[byte_index] = ord(string_input[current_position]) - 32
+                byte_index += 1
+                current_position += 1
 
         checksum = 0
-        for i, value in enumerate(bytes_output[:idx]):
-            if i == 0:
+        for current_position, value in enumerate(byte_array[:byte_index]):
+            if current_position == 0:
                 checksum = value
-            checksum += i * value
+            checksum += current_position * value
         checksum %= 103
-        bytes_output[idx] = checksum
-        idx += 1
-        bytes_output[idx] = 106
-        idx += 1
+        byte_array[byte_index] = checksum
+        byte_index += 1
+        byte_array[byte_index] = 106
+        byte_index += 1
 
         # Convert to Barcode 128 string
-        code128 = "".join(self.C128CHARS[value] for value in bytes_output[:idx])
+        code128 = "".join(self.C128CHARS[value] for value in byte_array[:byte_index])
 
         return code128
